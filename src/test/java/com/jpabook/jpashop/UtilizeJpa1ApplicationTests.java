a9dca0cad39a6e0e644b3f7c55a98836bb45b980
kokojihye <jihyevivianako@gmail.com>
2023-11-28 13:39:56 +0900

    Initial commit

---
+++ src/test/java/com/jpabook/jpashop/UtilizeJpa1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpabook.jpashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtilizeJpa1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
